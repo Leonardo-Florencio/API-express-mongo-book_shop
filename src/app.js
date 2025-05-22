ae044605271a887ec65ec01af95e0f1f8297b6ee
Leonardo-Florencio <leonardo_florenciop@outlook.com>
2025-05-22 20:47:22 -0300

    Demais métodos do CRUD definidos - temporário devido aos dados não persistirem ainda

--- src/app.js
+++ src/app.js
@@ -14,6 +14,12 @@ const livros = [
     }
 ];
 
+function buscaLivro(id){
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    })
+}
+
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js"); //send é usado para dados mais simples, como texto
 });
@@ -22,9 +28,26 @@ app.get("/livros",(req, res) => {
     res.status(200).json(livros); //json é usado para passar informações do tipo json (variável é uma lista de objetos, estrutura do json)
 });
 
+app.get("/livros/:id", (req, res) =>{ //os dois pontos servem para indicar que o id vai ser dinâmico
+    const index = buscaLivro(req.params.id)
+    res.status(200).json(livros[index])
+})
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso!")
 });
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros)
+})
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro deletado com sucesso")
+})
 
 export default app
